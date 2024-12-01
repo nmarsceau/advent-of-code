c0ba441dc39ea6b194c40aa2ace158973a57dfec
Nick Marsceau <nickmarsceau@gmail.com>
2024-12-01 14:47:29 -0500

    Solution for 2021 day 7 part 2

---
+++ 2021/7_2/solution.test.js
@@ -0,0 +1,20 @@
+import { readFile } from "node:fs/promises"
+
+import { solution } from "./solution.js"
+
+async function getTestInput(filename) {
+	return readFile(filename, "utf8")
+		.then(content => {
+			const lines = content.trim().split("\n")
+			return lines
+		})
+}
+
+describe("tests", () => {
+	test("test input 1", async () => {
+		const EXPECTED = 168
+		return getTestInput("./input.test.dat")
+			.then(solution)
+			.then(result => expect(result).toBe(EXPECTED))
+	})
+})
