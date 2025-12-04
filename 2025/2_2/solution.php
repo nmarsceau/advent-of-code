9ef23c0c3c53beeef600e847ce6144de3a8ed00f
Nick Marsceau <nickmarsceau@gmail.com>
2025-12-03 22:54:36 -0500

    Solution for 2025 day 2 part 2

---
+++ 2025/2_2/solution.php
@@ -0,0 +1,21 @@
+<?php
+
+$input = file_get_contents("php://stdin");
+$ranges = preg_split('/[,\s]+/', trim($input));
+$invalidIDSum = 0;
+
+foreach ($ranges as $range) {
+	// Parse the range
+	preg_match('/^(\d+)-(\d+)$/', $range, $matches);
+	$start = (int) $matches[1];
+	$end = (int) $matches[2];
+
+	// Check for invalid IDs in the range
+	for ($i = $start; $i <= $end; $i++) {
+		if (preg_match('/^(\d+)\1+$/', (string) $i)) {
+			$invalidIDSum += $i;
+		}
+	}
+}
+
+echo $invalidIDSum . "\n";
