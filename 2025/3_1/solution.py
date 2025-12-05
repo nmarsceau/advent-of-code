ba06840b0ae58b2250a4736981213b87d2ecac64
Nick Marsceau <nickmarsceau@gmail.com>
2025-12-04 22:03:23 -0500

    Refactor to be a little cleaner

--- 2025/3_1/solution.py
+++ 2025/3_1/solution.py
@@ -1,20 +1,17 @@
 import fileinput
 
-def parseInput(lines: list[str]) -> list[str]:
-	return [stripped for line in lines if (stripped := line.strip())]
+def parseInput(lines: list[str]) -> list[list[int]]:
+	return [[int(c) for c in stripped] for line in lines if (stripped := line.strip())]
 
-def findLargestDigit(s: str) -> int | None:
-	digits = [int(ch) for ch in s]
+def maxOrNone(digits: list[int]) -> int | None:
 	return None if len(digits) == 0 else max(digits)
 
-joltageSum = 0
+def findMaxJoltage(bank: list[int]) -> int:
+	largest = maxOrNone(bank)
+	largestIndex = bank.index(largest)
+	before = maxOrNone(bank[0:largestIndex]) or ''
+	after = maxOrNone(bank[largestIndex+1::]) or ''
+	return max(int(f"{before}{largest}"), int(f"{largest}{after}"))
 
-for bank in parseInput(fileinput.input()):
-	largest = findLargestDigit(bank)
-	largestIndex = bank.find(str(largest))
-	before = findLargestDigit(bank[0:largestIndex]) or ''
-	after = findLargestDigit(bank[largestIndex+1::]) or ''
-
-	joltageSum += max(int(f"{before}{largest}"), int(f"{largest}{after}"))
-
+joltageSum = sum([findMaxJoltage(bank) for bank in parseInput(fileinput.input())])
 print(joltageSum)
