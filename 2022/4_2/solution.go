61492954ad8afe4d74580465b489f3295a68d601
Nick Marsceau <nickmarsceau@gmail.com>
2026-06-14 16:57:58 -0400

    Solution for 2022 day 4 part 2

---
+++ 2022/4_2/solution.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type assignment struct {
+	start, end int
+}
+
+func (a *assignment) overlaps(b *assignment) bool {
+	return a.end >= b.start && a.start <= b.end
+}
+
+func newAssignment(input string) assignment {
+	parts := strings.Split(input, "-")
+	return assignment{
+		start: parseInt(parts[0]),
+		end:   parseInt(parts[1]),
+	}
+}
+
+type pair struct {
+	a, b assignment
+}
+
+func newPair(input string) pair {
+	parts := strings.Split(input, ",")
+	return pair{
+		a: newAssignment(parts[0]),
+		b: newAssignment(parts[1]),
+	}
+}
+
+func parseInt(input string) int {
+	converted, err := strconv.Atoi(input)
+	if err != nil {
+		panic("Unexpected error occurred converting to integer.")
+	}
+	return converted
+}
+
+func getInput() *[]string {
+	scanner := bufio.NewScanner(os.Stdin)
+	input := []string{}
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic("Unexpected error occurred reading from stdin.")
+	}
+	return &input
+}
+
+func parsePairs(input *[]string) *[]pair {
+	pairs := []pair{}
+	for _, line := range *input {
+		if line != "" {
+			pairs = append(pairs, newPair(line))
+		}
+	}
+	return &pairs
+}
+
+func main() {
+	pairs := parsePairs(getInput())
+
+	pairsFullyContained := 0
+	for _, pair := range *pairs {
+		if pair.a.overlaps(&pair.b) {
+			pairsFullyContained++
+		}
+	}
+	fmt.Println(pairsFullyContained)
+}
